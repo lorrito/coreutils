1a0bb283811414f8f006a67f9bc2313e6bfc6455
lorrinho <contato.lorransoares@gmail.com>
2024-03-23 13:32:06 -0300

    feat: first version of grep (a bad one)

--- programs/grep/src/main.rs
+++ programs/grep/src/main.rs
@@ -1,6 +1,20 @@
-use std::env;
+use std::{env, fs};
+
+fn filter(text: &str, filter: &str) {
+    for line in text.lines() {
+        if line.contains(filter) {
+            println!("{}", line);
+        }
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+    let text = fs::read_to_string(&args[1]);
+    match text {
+        Ok(text_content) => {
+            filter(&text_content, &args[2])
+        },
+        Err(e) => eprintln!("{}", e),
+    }
 }
